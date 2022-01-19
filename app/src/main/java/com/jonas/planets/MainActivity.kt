f6bdcf3bf57ff873302811c65979319e4e55e40a
Jonas Sinkevicius <jonis@Jonass-MacBook-Pro.local>
2022-01-19 03:00:28 +0000

    Initial commit

---
+++ app/src/main/java/com/jonas/planets/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.jonas.planets
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import com.jonas.planets.databinding.ActivityMainBinding
+
+
+val planetsArray = arrayOf(
+            "Mercury",
+            "Venus" ,
+            "Earth" )
+
+         //   "Mars" ,
+         //   "Saturn",
+         //   "Jupiter" ,
+        //    "Uranus" ,
+        //    "Neptune"
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val viewPager = binding.viewPager
+        val tabLayout = binding.tabLayout
+
+        tabLayout.tabGravity = TabLayout.GRAVITY_CENTER
+        tabLayout.tabMode = TabLayout.MODE_AUTO
+
+        val adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
+        viewPager.adapter = adapter
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = planetsArray[position]
+        }.attach()
+    }
+}
+
+
---
+++ app/src/main/java/com/jonas/planets/ViewPagerAdapter.kt
@@ -0,0 +1,88 @@
+package com.jonas.planets
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.jonas.planets.databinding.FragmentP1Binding
+import com.jonas.planets.databinding.FragmentP2Binding
+import com.jonas.planets.databinding.FragmentP3Binding
+
+
+private const val NUM_TABS = 3
+
+class ViewPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
+        FragmentStateAdapter(fragmentManager, lifecycle) {
+
+    override fun getItemCount(): Int {
+        return NUM_TABS
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        when (position) {
+            0 -> return P1Fragment()
+            1 -> return P2Fragment()
+        }
+        return P3Fragment()
+    }
+
+}
+
+class P1Fragment : Fragment() {
+    private var _binding: FragmentP1Binding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentP1Binding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
+
+class P2Fragment : Fragment() {
+    private var _binding: FragmentP2Binding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentP2Binding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
+
+class P3Fragment : Fragment() {
+    private var _binding: FragmentP3Binding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentP3Binding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
+
